b605b53dd7cd13023c60131b29bb53c47af8c732
q-digital <q-digital.org>
2020-07-12 23:53:23 +0400

    use test range input

--- src/game_one/js/components/GameOne.vue
+++ src/game_one/js/components/GameOne.vue
@@ -3,13 +3,24 @@
     <Progress />
     <GameScreen dir="left">
       <div class="game__block">
-        <Slider :data="[[1, 2, 3 ,4]]" />
+        <range-slider
+            class="slider"
+            min="1"
+            max="1000"
+            step="1">
+        </range-slider>
       </div>
       <div class="game__block">
         <Input type="text" placeholder="Enter number" />
       </div>
     </GameScreen>
     <GameScreen dir="right">
+    <div class="game__block">
+        <Slider :data="[[1, 2, 3 ,4]]" />
+      </div>
+      <div class="game__block">
+        <Input type="text" placeholder="Enter number" />
+      </div>
     </GameScreen>
   </div>
 </template>;
@@ -19,9 +30,11 @@ import GameScreen from '@/js/components/GameScreen.vue';
 import Progress from '@/js/components/Progress.vue';
 import Slider from '@/js/components/Slider.vue';
 import Input from '@/js/components/Input.vue';
+import RangeSlider from 'vue-range-slider'
+import 'vue-range-slider/dist/vue-range-slider.css'
 
 export default {
   name: 'game-one',
-  components: {GameScreen, Progress, Slider, Input}
+  components: {GameScreen, Progress, Slider, Input, RangeSlider}
 }
 </script>
