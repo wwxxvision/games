188d1b878688de460e666e706a9e32dfb34b9588
Рустам Батршин <r.batrshin@russianit.ru>
2020-08-20 18:51:26 +0000

    Created custom range input

--- src/js/components/Range.vue
+++ src/js/components/Range.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="range">
-		  <vue-slider
+		  <!-- <vue-slider
 			:disabled="disabled"
 			:direction="$direction"
 			:processStyle="{
@@ -8,7 +8,11 @@
 				borderRadius: 2,
 				backgroundColor: '#EF4141'
 			}"
-			@change="updated" :min="1" :max="1000" tooltip="none" v-model="value" />
+			@change="updated" :min="1" :max="1000" tooltip="none" v-model="value" /> -->
+			<div :class="{'range__custom': true, [$direction]: true}">
+				<div ref="dot" class="range__dot"></div>
+			</div>
+			<input @input="updated" v-model="value" class="range__input" type="range" :disabled="disabled" :max="1000" :min="1" :step="1" />
 	</div>
 </template>
 
@@ -41,11 +45,20 @@ export default {
 			if (!this.disabled) {
 				this.$emit('updateValues', this.playerType, this.value);
 			}
-		}
+		},
 	},
 	watch: {
 		initValue(value) {
 			this.value = value;
+		},
+		value(val) {
+			let value = (val * 100) / 1000;
+			if (value > 85) {
+					this.$refs.dot.style.right = `calc(${value}% - 15px)`
+			}
+			else {
+					this.$refs.dot.style.right = `calc(${value}%)`
+			}
 		}
 	}
 };
