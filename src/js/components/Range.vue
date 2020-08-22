2509170c96e424d936deaedc31a79a445c6594f0
Рустам Батршин <r.batrshin@russianit.ru>
2020-08-22 09:46:33 +0000

    use native input range

--- src/js/components/Range.vue
+++ src/js/components/Range.vue
@@ -1,33 +1,16 @@
 <template>
 	<div class="range">
-		  <!-- <vue-slider
-			:disabled="disabled"
-			:direction="$direction"
-			:processStyle="{
-				height: '9px',
-				borderRadius: 2,
-				backgroundColor: '#EF4141'
-			}"
-			@change="updated" :min="1" :max="1000" tooltip="none" v-model="value" /> -->
-			<div :class="{'range__custom': true, [$direction]: true}">
-				<div ref="dot" class="range__dot"></div>
-			</div>
-			<input @input="updated" v-model="value" class="range__input" type="range" :disabled="disabled" :max="1000" :min="1" :step="1" />
+		<input @input="updated" v-model="value" class="range__item" type="range" :disabled="disabled" :max="1000" :min="1" :step="1" />
 	</div>
 </template>
 
 <script>
-import VueSlider from 'vue-slider-component'
-import 'vue-slider-component/theme/antd.css'
 
 export default {
 	data() {
 		return {
 			value: 1,
 		};
-	},
-	components: {
-		VueSlider
 	},
 	props: {
 		disabled: {
@@ -51,15 +34,6 @@ export default {
 		initValue(value) {
 			this.value = value;
 		},
-		value(val) {
-			let value = (val * 100) / 1000;
-			if (value > 85) {
-					this.$refs.dot.style.right = `calc(${value}% - 15px)`
-			}
-			else {
-					this.$refs.dot.style.right = `calc(${value}%)`
-			}
-		}
 	}
 };
 </script>
--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VModal from 'vue-js-modal';
 import { VueHammer } from 'vue2-hammer';
+import 'nouislider/distribute/nouislider.css';
 
 Vue.use(VueHammer);
 Vue.use(VModal);
